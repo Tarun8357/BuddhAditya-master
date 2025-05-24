71654531d788d9d4c70e1a34a4cadcfb4013dae2
Tarun8357 <tarun.dhakad1205@gmail.com>
2025-05-24 15:36:58 +0530

    Added : Endpoints added in LabTestManager

---
+++ microservices/lawsuite/casemanager/src/main/java/com/buddhaditya/casemanager/CasemanagerApplication.java
@@ -0,0 +1,13 @@
+package com.buddhaditya.casemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasemanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasemanagerApplication.class, args);
+	}
+
+}
