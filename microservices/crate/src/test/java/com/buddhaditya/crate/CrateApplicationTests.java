a1fedde969dc87e1b73d3ac9f1483078e1989f06
Tarun8357 <tarun.dhakad1205@gmail.com>
2025-09-19 12:30:40 +0530

    Fixed : Name of the repos fixed.

---
+++ microservices/crate/src/test/java/com/buddhaditya/crate/CrateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buddhaditya.crate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ microservices/obsidian/src/main/java/com/buddhaditya/obsidian/ObsidianApplication.java
@@ -0,0 +1,13 @@
+package com.buddhaditya.obsidian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObsidianApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObsidianApplication.class, args);
+	}
+
+}
